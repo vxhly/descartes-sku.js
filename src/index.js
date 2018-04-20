ce4b2acee29d467159f2b07f5de41ebd0d1186ab
vxhly <pengchengou@gmail.com>
2018-04-20 17:27:22 +0800

    feat(init):

---
+++ src/index.js
@@ -0,0 +1,9 @@
+class Descartes {
+  constructor (arr) {
+    this._arr = arr
+  }
+  descartes () {
+    console.log(this._arr)
+  }
+}
+export default Descartes
---
+++ test/test.js
@@ -0,0 +1,5 @@
+const Descartes = require('../src/index.js')
+
+const arr = ['a', 'b', 'c']
+const descartes = new Descartes(arr)
+descartes.descartes()
